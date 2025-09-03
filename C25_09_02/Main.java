2f5b49e5e0153738d4c068b9b3c23ac6b6307758
나의찬 <cab07036@naver.com>
2025-09-03 19:42:08 +0900

    250902 문제 10828

--- C25_09_02/Main.java
+++ C25_09_02/Main.java
@@ -6,7 +6,10 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-/* 정수를 저장하는 스택을 구현한 다음, 
+/* 
+문제 10828
+
+정수를 저장하는 스택을 구현한 다음, 
 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
 
 명령은 총 다섯 가지이다.
