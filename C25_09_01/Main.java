4cfbf180df2cf9fef66cbe39259c8d71ace73a9d
나의찬 <cab07036@naver.com>
2025-09-03 19:41:58 +0900

    250901 문제 9012

--- C25_09_01/Main.java
+++ C25_09_01/Main.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /* 
+문제 9012
+
 괄호 문자열(Parenthesis String, PS)은 두 개의 괄호 기호인 ‘(’ 와 ‘)’ 만으로 구성되어 있는 문자열이다. 
 그 중에서 괄호의 모양이 바르게 구성된 문자열을 올바른 괄호 문자열(Valid PS, VPS)이라고 부른다. 
 한 쌍의 괄호 기호로 된 “( )” 문자열은 기본 VPS 이라고 부른다. 
